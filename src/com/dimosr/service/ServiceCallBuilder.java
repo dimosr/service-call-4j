73b22877dba830a14ca3fb9821bf1d161ff97c17
Raptis Dimos <raptis.dimos@yahoo.gr>
2017-05-13 15:02:39 +0100

    Added monitoring capabilities in ServiceCallBuilder

--- src/com/dimosr/service/ServiceCallBuilder.java
+++ src/com/dimosr/service/ServiceCallBuilder.java
@@ -3,9 +3,37 @@ package com.dimosr.service;
 import com.dimosr.service.core.Cache;
 import com.dimosr.service.core.ServiceCall;
 
+import java.time.Clock;
+import java.time.Duration;
+import java.util.function.Consumer;
+
+/**
+ * A builder used to enhance a ServiceCall with additional capabilities.
+ * Currently, the available capabilities are the following:
+ * - caching
+ * - monitoring (latency)
+ *
+ * The capabilities are built on top of the provided ServiceCall.
+ * The layering is such that there is no interference between the various capabilities.
+ * The layering on top of the ServiceCall is the following:
+ *
+ * -------------------------------------------------
+ * |                 Caching                       |
+ * -------------------------------------------------
+ * |                Monitoring                     |
+ * -------------------------------------------------
+ * |                ServiceCall                    |
+ * -------------------------------------------------
+ *                      |
+ *                      |
+ *                   -------
+ *                   Service
+ */
 public class ServiceCallBuilder<REQUEST, RESPONSE> {
     private ServiceCall<REQUEST, RESPONSE> enhancedServiceCall;
+
     private Cache<REQUEST, RESPONSE> cache;
+    private Consumer<Duration> latencyConsumer;
 
     public ServiceCallBuilder(final ServiceCall<REQUEST, RESPONSE> serviceCall) {
         this.enhancedServiceCall = serviceCall;
@@ -16,11 +44,27 @@ public class ServiceCallBuilder<REQUEST, RESPONSE> {
         return this;
     }
 
+    public ServiceCallBuilder<REQUEST, RESPONSE> withMonitoring(final Consumer<Duration> latencyConsumer) {
+        this.latencyConsumer = latencyConsumer;
+        return this;
+    }
+
     public ServiceCall<REQUEST, RESPONSE> build() {
+        wrapWithMonitoring();
+        wrapInCache();
+
+        return enhancedServiceCall;
+    }
+
+    private void wrapInCache() {
         if(cache != null) {
             enhancedServiceCall = new CachedServiceCall<>(enhancedServiceCall, cache);
         }
+    }
 
-        return enhancedServiceCall;
+    private void wrapWithMonitoring() {
+        if(latencyConsumer != null) {
+            enhancedServiceCall = new MonitoredServiceCall<>(enhancedServiceCall, Clock.systemUTC(), latencyConsumer);
+        }
     }
 }
--- test/com/dimosr/service/ServiceCallBuilderTest.java
+++ test/com/dimosr/service/ServiceCallBuilderTest.java
@@ -9,8 +9,11 @@ import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.time.Duration;
 import java.util.Optional;
+import java.util.function.Consumer;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -21,6 +24,10 @@ public class ServiceCallBuilderTest {
 
     @Mock
     private ServiceCall<String, String> originalServiceCall;
+    @Mock
+    private Cache<String, String> cache;
+    @Mock
+    private Consumer<Duration> latencyConsumer;
 
     private final static String SAMPLE_REQUEST = "input";
     private final static String SAMPLE_RESPONSE = "input with some more data";
@@ -29,6 +36,8 @@ public class ServiceCallBuilderTest {
     public void setupOriginalServiceResponses() {
         when(originalServiceCall.call(SAMPLE_REQUEST))
                 .thenReturn(SAMPLE_RESPONSE);
+        when(cache.get(SAMPLE_REQUEST))
+                .thenReturn(Optional.empty());
     }
 
     @Test
@@ -42,19 +51,29 @@ public class ServiceCallBuilderTest {
 
     @Test
     public void testBuildingServiceWithCache() {
-        Cache<String, String> cache = mock(Cache.class);
-        when(cache.get(SAMPLE_REQUEST))
-                .thenReturn(Optional.empty());
-
-
         ServiceCall<String, String> enhancedServiceCall = new ServiceCallBuilder<>(originalServiceCall)
                 .withCache(cache)
                 .build();
 
-        String response = enhancedServiceCall.call(SAMPLE_REQUEST);
+        enhancedServiceCall.call(SAMPLE_REQUEST);
 
         InOrder orderSensitiveMocks = inOrder(originalServiceCall, cache);
         orderSensitiveMocks.verify(cache).get(SAMPLE_REQUEST);
         orderSensitiveMocks.verify(originalServiceCall).call(SAMPLE_REQUEST);
+    }
+
+    @Test
+    public void testBuildingServiceWithCacheAndMonitoring() {
+        ServiceCall<String, String> enhancedServiceCall = new ServiceCallBuilder<>(originalServiceCall)
+                .withCache(cache)
+                .withMonitoring(latencyConsumer)
+                .build();
+
+        enhancedServiceCall.call(SAMPLE_REQUEST);
+
+        InOrder orderSensitiveMocks = inOrder(originalServiceCall, cache, latencyConsumer);
+        orderSensitiveMocks.verify(cache).get(SAMPLE_REQUEST);
+        orderSensitiveMocks.verify(originalServiceCall).call(SAMPLE_REQUEST);
+        orderSensitiveMocks.verify(latencyConsumer).accept(any(Duration.class));
     }
 }
